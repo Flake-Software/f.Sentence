ed81a5e78a9a8de883c09606e75caf6147a8f12b
Zoran Jeremić <zoki@tutanota.de>
2026-02-23 20:44:31 +0100

    Implement DocxExtractor to extract document.xml

--- lib/features/docx_extractor.dart
+++ lib/features/docx_extractor.dart
@@ -1,1 +1,18 @@
+import 'dart:io';
+import 'package:archive/archive_io.dart';
 
+class DocxExtractor {
+  static String extractDocumentXml(String filePath) {
+    final bytes = File(filePath).readAsBytesSync();
+    final archive = ZipDecoder().decodeBytes(bytes);
+
+    for (final file in archive) {
+      if (file.isFile && file.name == 'word/document.xml') {
+        final data = file.content as List<int>;
+        return String.fromCharCodes(data);
+      }
+    }
+
+    throw Exception('Could not find document.xml.');
+  }
+}
