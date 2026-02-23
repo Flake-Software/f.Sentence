79a7c0b79d4022d48c736d7526157a4bba1286bf
Zoran Jeremić <zoki@tutanota.de>
2026-02-23 20:45:09 +0100

    Implement DocxParser to parse XML documents

--- lib/features/docx_parser.dart
+++ lib/features/docx_parser.dart
@@ -1,1 +1,21 @@
+import 'package:xml/xml.dart';
+import '../../core/document_model.dart';
 
+class DocxParser {
+  static Document parse(String xmlString) {
+    final doc = Document();
+    final document = XmlDocument.parse(xmlString);
+    final paragraphsXml = document.findAllElements('w:p');
+
+    for (var p in paragraphsXml) {
+      final para = Paragraph();
+      final spans = p.findAllElements('w:t');
+      for (var s in spans) {
+        para.spans.add(Span(s.text));
+      }
+      doc.paragraphs.add(para);
+    }
+
+    return doc;
+  }
+}
