767e51bfde74a415fdf95854c1f6e2ebd0652fec
Zoran Jeremić <zoki@tutanota.de>
2026-02-23 20:46:17 +0100

    Add DocumentService to load and parse DOCX files

--- lib/core/document_service.dart
+++ lib/core/document_service.dart
@@ -1,1 +1,11 @@
+import '../features/docx_reader/docx_extractor.dart';
+import '../features/docx_reader/docx_parser.dart';
+import 'document_model.dart';
 
+class DocumentService {
+  static Document loadDocx(String path) {
+    final xmlString = DocxExtractor.extractDocumentXml(path);
+    final document = DocxParser.parse(xmlString);
+    return document;
+  }
+}
