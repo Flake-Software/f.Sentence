3c4c722d253ab6645af2fbc4a95818295aec74f1
Zoran Jeremić <batajeremiccc@gmail.com>
2026-02-24 11:28:38 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'ui/screens/home.dart';
+import 'ui/screens/onboarding.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
-
-  final prefs = await SharedPreferences.getInstance();
-  final completed = prefs.getBool('onboarding_complete') ?? false;
-
-  runApp(
-    EasyLocalization(
-      supportedLocales: const [Locale('en')],
-      path: 'assets/lang',
-      fallbackLocale: const Locale('en'),
-      child: MyApp(showHome: completed),
-    ),
-  );
+void main() {
+  runApp(const FSentenceApp());
 }
 
-class MyApp extends StatelessWidget {
-  final bool showHome;
-
-  const MyApp({super.key, required this.showHome});
+class FSentenceApp extends StatelessWidget {
+  const FSentenceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'f.Sentence',
-      debugShowCheckedModeBanner: false,
-      locale: context.locale,
-      supportedLocales: context.supportedLocales,
-      localizationsDelegates: context.localizationDelegates,
-      initialRoute: showHome ? '/main' : '/onboarding',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        fontFamily: 'Roboto', // tvoj izbor
+      ),
+      initialRoute: '/onboarding',
       routes: {
         '/main': (context) => const HomeScreen(),
         '/onboarding': (context) => const OnboardingScreen(),
