b41c707d22339678c3d8d42b41221e37fed8dd2b
Zoran Jeremić <batajeremiccc@gmail.com>
2026-02-24 12:24:23 +0100

    Update onboarding.dart

--- lib/ui/screens/onboarding.dart
+++ lib/ui/screens/onboarding.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
+  Future<void> finishOnboarding(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('seenOnboarding', true);
+    Navigator.pushReplacementNamed(context, '/main');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Onboarding'),
-      ),
+      appBar: AppBar(title: const Text('Onboarding')),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            // Navigacija ka HomeScreen
-            Navigator.pushNamed(context, '/main');
-          },
-          child: const Text('Go to Home'),
+          onPressed: () => finishOnboarding(context),
+          child: const Text('Finish Onboarding'),
         ),
       ),
     );
