6b6d4496629c3c20846c69e3d9708765d30a4fdc
Zoran Jeremić <zoki@tutanota.de>
2026-03-07 20:37:26 +0100

    Enhance StorageService with file management methods
    Added methods to retrieve, delete, and manage local text files.

--- lib/core/storage_service.dart
+++ lib/core/storage_service.dart
@@ -2,30 +2,54 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 
 class StorageService {
-  // Metoda da dobijemo putanju do foldera gde su dokumenti
   static Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
     return directory.path;
   }
-
-  // Metoda za čuvanje fajla po imenu
+  static Future<List<File>> getLocalFiles() async {
+    try {
+      final path = await _localPath;
+      final directory = Directory(path);
+      
+      final List<FileSystemEntity> entities = await directory.list().toList();
+      
+      final List<File> files = entities
+          .whereType<File>()
+          .where((file) => file.path.endsWith('.txt'))
+          .toList();
+      files.sort((a, b) => b.lastModifiedSync().compareTo(a.lastModifiedSync()));
+      
+      return files;
+    } catch (e) {
+      return [];
+    }
+  }
   static Future<void> saveFile(String fileName, String content) async {
     final path = await _localPath;
     final file = File('$path/$fileName');
     await file.writeAsString(content);
   }
-
-  // Metoda za čitanje fajla
   static Future<String> readFile(String fileName) async {
     try {
       final path = await _localPath;
       final file = File('$path/$fileName');
+      
       if (await file.exists()) {
         return await file.readAsString();
       }
       return "";
     } catch (e) {
       return "";
+    }
+  }
+  static Future<void> deleteFile(String fileName) async {
+    try {
+      final path = await _localPath;
+      final file = File('$path/$fileName');
+      if (await file.exists()) {
+        await file.delete();
+      }
+    } catch (e) {
     }
   }
 }
