8fadc7e94ee2a3618094b457a5d3d509f47b08ca
Zoran <batajeremiccc@gmail.com>
2026-04-12 20:43:21 +0200

    Update MainActivity.kt [build]

--- android/app/src/main/kotlin/com/flake/sentence/MainActivity.kt
+++ android/app/src/main/kotlin/com/flake/sentence/MainActivity.kt
@@ -1,5 +1,20 @@
 package com.flake.sentence
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import android.os.Build
+import android.view.View
+import android.view.WindowInsets
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        // Forsiranje Edge-to-Edge na nivou sistema
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.navigationBarColor = 0 // Transparentna
+            window.statusBarColor = 0 // Transparentna
+        }
+    }
+}
