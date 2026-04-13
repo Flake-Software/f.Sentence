59f6c54c849bcd668e2837506147f59db9dbc8be
Zoran <batajeremiccc@gmail.com>
2026-04-13 19:59:44 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 21
         targetSdk = 36
         versionCode = 2
-        versionName = "0.8.7-beta"
+        versionName = "1.0.0-canary"
     }
 
     signingConfigs {
