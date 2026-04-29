42d7139a156f24fa2b20f9d5ae4fe97226ec8b74
Zoran <zoki@tutanota.de>
2026-04-29 17:56:01 +0200

    Add AddNoteWidgetProvider for note widget functionality

---
+++ android/app/src/main/kotlin/com/flake/sentence/AddNoteWidgetProvider.kt
@@ -0,0 +1,28 @@
+package com.flake.sentence
+
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.Uri
+import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetProvider
+
+class AddNoteWidgetProvider : HomeWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray,
+        widgetData: SharedPreferences
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            val views = RemoteViews(context.packageName, R.layout.add_note_widget).apply {
+                val pendingIntent = HomeWidgetProvider.getPendingIntent(
+                    context,
+                    Uri.parse("home_widget://add_note")
+                )
+                setOnClickPendingIntent(R.id.widget_root, pendingIntent)
+            }
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
