0296015ae72facf219ec72f6e866e0f041ad3ed3
Zoran <batajeremiccc@gmail.com>
2026-05-06 20:09:40 +0200

    Update widget_manager.dart

--- lib/core/widgets/widget_manager.dart
+++ lib/core/widgets/widget_manager.dart
@@ -1,23 +1,11 @@
 import 'package:home_widget/home_widget.dart';
-import 'package:flutter/material.dart';
 
 class WidgetManager {
-  static const String androidWidgetName = 'AddNoteWidget'; // Must match Android XML
-  static const String addNoteGroup = 'group.sentence.widgets';
+  static const String appGroupId = 'group.sentence.widgets';
+  static const String uriScheme = 'sentence://add_note';
 
-  /// Sets up the listener for widget clicks
   static Future<void> setupWidgetClickListener(Function(Uri?) callback) async {
-    HomeWidget.setAppGroupId(addNoteGroup);
-    HomeWidget.registerInteractivityCallback(interactiveCallback);
-    
-    // Listen for the URI that triggered the app open
-    HomeWidget.initiallyLaunchedFromHomeWidget().then(callback);
+    await HomeWidget.setAppGroupId(appGroupId);
     HomeWidget.widgetClicked.listen(callback);
-  }
-
-  /// This is called when the widget is clicked while app is in background
-  @pragma('vm:entry-point')
-  static Future<void> interactiveCallback(Uri? uri) async {
-    // Actions can be handled here if needed for background tasks
   }
 }
