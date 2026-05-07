8a1c2f8f0833812e840dff052091b12c775445b4
Zoran <batajeremiccc@gmail.com>
2026-05-07 09:49:05 +0200

    Update AddNoteWidgetProvider.kt

--- android/app/src/main/kotlin/com/flake/sentence/AddNoteWidgetProvider.kt
+++ android/app/src/main/kotlin/com/flake/sentence/AddNoteWidgetProvider.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.net.Uri
 import android.widget.RemoteViews
+import android.content.Intent
+import android.app.PendingIntent
 import es.antonborri.home_widget.HomeWidgetLaunchIntent
 import es.antonborri.home_widget.HomeWidgetProvider
 
@@ -17,11 +19,21 @@ class AddNoteWidgetProvider : HomeWidgetProvider() {
     ) {
         for (appWidgetId in appWidgetIds) {
             val views = RemoteViews(context.packageName, R.layout.add_note_widget).apply {
-                val pendingIntent = HomeWidgetLaunchIntent.getActivity(
-                    context,
-                    MainActivity::class.java,
-                    Uri.parse("sentence://add_note")
+                // Generišemo Intent sa flagovima koji osiguravaju da svaki klik bude "svež"
+                val intent = Intent(context, MainActivity::class.java).apply {
+                    data = Uri.parse("sentence://add_note?time=${System.currentTimeMillis()}")
+                    action = Intent.ACTION_VIEW
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                }
+
+                val pendingIntent = PendingIntent.getActivity(
+                    context, 
+                    appWidgetId, 
+                    intent, 
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
                 )
+
                 setOnClickPendingIntent(R.id.widget_root, pendingIntent)
             }
             appWidgetManager.updateAppWidget(appWidgetId, views)
