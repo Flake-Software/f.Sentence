76218f37fbe69be9fe8ceedc94cee7872a0c14a4
Zoran <batajeremiccc@gmail.com>
2026-05-11 14:25:16 +0200

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/flake/sentence/MainActivity.kt
+++ android/app/src/main/kotlin/com/flake/sentence/MainActivity.kt
@@ -2,16 +2,21 @@ package com.flake.sentence
 
 import io.flutter.embedding.android.FlutterActivity
 import android.os.Bundle
+import android.content.Intent
 import android.view.WindowManager
 import androidx.core.view.WindowCompat
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        
         super.onCreate(savedInstanceState)
-        
+
         window.navigationBarColor = android.graphics.Color.TRANSPARENT
         window.statusBarColor = android.graphics.Color.TRANSPARENT
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
     }
 }
